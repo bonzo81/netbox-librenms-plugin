1690e41f234b866a216ab2c227db504e288e8e19
Andy Norwood <andytnorwood@gmail.com>
2024-10-14 22:46:27 +0100

    Add mtu & mac_address to mappings, and function to format mac_address

--- netbox_librenms_plugin/utils.py
+++ netbox_librenms_plugin/utils.py
@@ -9,4 +9,25 @@ LIBRENMS_TO_NETBOX_MAPPING = {
     'ifType': 'type',
     'ifSpeed': 'speed',
     'ifAlias': 'description',
+    'ifPhysAddress': 'mac_address',
+    'ifMtu': 'mtu',
 }
+
+def format_mac_address(mac_address):
+    """
+    Validate and format MAC address string for table display.
+
+    Args:
+        mac_address (str): The MAC address string to format.
+
+    Returns:
+        str: The MAC address formatted as XX:XX:XX:XX:XX:XX. 
+    """
+    mac_address = mac_address.strip().replace(':', '').replace('-', '')
+
+    if len(mac_address) != 12:
+        return "Invalid MAC Address"  # Return a message if the address is not valid
+    
+    formatted_mac = ':'.join(mac_address[i:i+2] for i in range(0, len(mac_address), 2))
+    return formatted_mac.upper()
+
