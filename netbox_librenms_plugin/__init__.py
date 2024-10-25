a0b76a3d594899b475524f48b9c058c6ef5b6b89
Andy Norwood <andytnorwood@gmail.com>
2024-10-25 15:32:48 +0100

    Update to v0.2.6

--- netbox_librenms_plugin/__init__.py
+++ netbox_librenms_plugin/__init__.py
@@ -5,7 +5,7 @@ class LibreNMSSyncConfig(PluginConfig):
     name = "netbox_librenms_plugin"
     verbose_name = "NetBox Librenms Plugin"
     description = "Sync data from LibreNMS into NetBox"
-    version = "0.2.5"
+    version = "0.2.6"
     base_url = "librenms_plugin"
     required_settings = []
     default_settings = {"enable_caching": True}
