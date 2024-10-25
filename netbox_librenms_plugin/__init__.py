2dee63d4ac18f5f5c8a23635f161f2987cac562b
Andy Norwood <andytnorwood@gmail.com>
2024-10-25 15:17:31 +0100

    Remove unused import

--- netbox_librenms_plugin/__init__.py
+++ netbox_librenms_plugin/__init__.py
@@ -1,5 +1,4 @@
 from netbox.plugins import PluginConfig
-from netbox.plugins import get_plugin_config
 
 
 class LibreNMSSyncConfig(PluginConfig):
@@ -9,9 +8,7 @@ class LibreNMSSyncConfig(PluginConfig):
     version = "0.2.5"
     base_url = "librenms_plugin"
     required_settings = []
-    default_settings = {
-        'enable_caching': True
-    }
+    default_settings = {"enable_caching": True}
 
 
 config = LibreNMSSyncConfig
