6bcaa8e31192383e9b4d7b34416b972440152290
Andy Norwood <andy.norwood@nhslothian.scot.nhs.uk>
2024-11-11 15:38:47 +0000

    Bump version to 0.2.7 and update plugin description and required settings

--- netbox_librenms_plugin/__init__.py
+++ netbox_librenms_plugin/__init__.py
@@ -4,10 +4,12 @@ from netbox.plugins import PluginConfig
 class LibreNMSSyncConfig(PluginConfig):
     name = "netbox_librenms_plugin"
     verbose_name = "NetBox Librenms Plugin"
-    description = "Sync data from LibreNMS into NetBox"
-    version = "0.2.6"
+    description = "Netbox plugin to sync data between LibreNMS and Netbox."
+    author = "Andy Norwood"
+    version = "0.2.7"
     base_url = "librenms_plugin"
-    required_settings = []
+    min_version = '4.1.0'
+    required_settings = ["librenms_url", "api_token"]
     default_settings = {"enable_caching": True, "verify_ssl": True}
 
 
