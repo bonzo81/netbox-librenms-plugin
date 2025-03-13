15c38361a95cd7b2a94bbe38045b4448cb1f66bc
Andy Norwood <andy.norwood@nhslothian.scot.nhs.uk>
2025-03-13 13:50:15 +0000

    Fix missing add_device_modal.html template and rework add device to Librenms form

--- netbox_librenms_plugin/forms.py
+++ netbox_librenms_plugin/forms.py
@@ -42,9 +42,6 @@ class AddToLIbreSNMPV2(forms.Form):
         required=True,
         widget=forms.TextInput(attrs={"id": "id_hostname_v2"}),
     )
-    snmp_version = forms.CharField(
-        widget=forms.HiddenInput(attrs={"id": "id_snmp_version_v2"})
-    )
     community = forms.CharField(label="SNMP Community", max_length=255, required=True)
 
 
@@ -59,9 +56,6 @@ class AddToLIbreSNMPV3(forms.Form):
         max_length=255,
         required=True,
         widget=forms.TextInput(attrs={"id": "id_hostname_v3"}),
-    )
-    snmp_version = forms.CharField(
-        widget=forms.HiddenInput(attrs={"id": "id_snmp_version_v3"}), initial="v3"
     )
     authlevel = forms.ChoiceField(
         label="Auth Level",
