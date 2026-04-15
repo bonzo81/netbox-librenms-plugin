2763a2fb9f3b4df7c76952965e94a0997d6e5d7a
Andy Norwood <andytnorwood@gmail.com>
2026-04-15 10:13:17 +0000

    chore: bump version to 0.4.4

--- netbox_librenms_plugin/__init__.py
+++ netbox_librenms_plugin/__init__.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ImproperlyConfigured
 from netbox.plugins import PluginConfig
 
 __author__ = "Andy Norwood"
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 
 
 class LibreNMSSyncConfig(PluginConfig):
