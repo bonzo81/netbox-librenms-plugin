4d40ec6efd8fd4c9904fb1e96f25cad1e647547b
Marcin Zieba <marcinpsk@gmail.com>
2026-08-06 18:16:21 +0200

    fix(serial): restore constraint trim import

--- netbox_librenms_plugin/models.py
+++ netbox_librenms_plugin/models.py
@@ -10,7 +10,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import Q
-from django.db.models.functions import Lower
+from django.db.models.functions import Lower, Trim
 from django.urls import reverse
 from netbox.models import NetBoxModel
 from utilities.fields import ColorField
